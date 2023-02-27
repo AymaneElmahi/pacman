731cb9d1ab4379b0263ca32ea3fde32896368b0e
aelmahi <elmahi.aymane@gmail.com>
2023-02-27 17:39:26 +0100

    added map to window

--- window.h
+++ window.h
@@ -2,19 +2,21 @@
 #define WINDOW_H
 
 #include "utils.h"
+#include "map.h"
+
 
 class Window {
 public:
     Window();
     ~Window();
+    void insertMap();
     // void handle_events();
     // void render();
-    // void run();
+    void run();
 
 protected:
     SDL_Window* window_;
-    SDL_Surface *plancheSprites_;
-    // Map map_;
+    Map * map_;
     // Pacman pacman_;
     // vector<Ghost> ghosts_;
     // vector<Dot> dots_;
