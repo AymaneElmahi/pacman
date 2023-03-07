cab9ee7a5fb90e6616ec37427120957de8b357d1
AymaneElmahi <elmahi.aymane@gmail.com>
2023-03-07 23:24:56 +0100

    need to checkout

--- inc/ghost.h
+++ inc/ghost.h
@@ -12,10 +12,7 @@ public:
     // getters
     int getDirection() const;
     int getSpeed() const;
-    int getFrame() const;
-    int getFrameMax() const;
-    int getFrameTimer() const;
-    int getFrameTimerMax() const;
+
     SDL_Surface *getPlancheSprites() const;
     SDL_Rect *getSrcRect() const;
     SDL_Rect *getDestRect() const;
@@ -23,10 +20,7 @@ public:
     // setters
     void setDirection(int direction);
     void setSpeed(int speed);
-    void setFrame(int frame);
-    void setFrameMax(int frameMax);
-    void setFrameTimer(int frameTimer);
-    void setFrameTimerMax(int frameTimerMax);
+
     void setPlancheSprites(SDL_Surface *plancheSprites);
     void setSrcRect(SDL_Rect *srcRect);
     void setDestRect(SDL_Rect *destRect);
@@ -35,10 +29,7 @@ public:
 protected:
     int direction_;
     int speed_;
-    int frame_;
-    int frameMax_;
-    int frameTimer_;
-    int frameTimerMax_;
+    
     SDL_Surface *plancheSprites_;
     SDL_Rect *srcRect_;
     SDL_Rect *destRect_;
